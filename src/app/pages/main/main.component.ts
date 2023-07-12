2a81d4f2a30a5b956b94a9129dff8b98d49bc89d
Armando Herrera <armandoher01@gmail.com>
2023-07-12 07:20:49 -0300

    main page

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { timer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  public spinner: boolean = false;
+
+  private timer: Subscription = new Subscription();
+
+  constructor() {
+    console.log('MainComponent.constructor()');
+    this.spinner = true;
+  }
 
   ngOnInit(): void {
+    console.log('MainComponent.ngOnInit()');
+
+    const source = timer(2000);
+    this.timer = source.subscribe(val => this.spinner = false);
+  }
+
+
+  ngOnDestroy(): void {
+    console.log('MainComponent.ngOnDestroy()');
+    this.timer.unsubscribe();
   }
 
 }
